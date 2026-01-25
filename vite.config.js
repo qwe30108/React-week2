47f0f2e0e0e11e568b5975607434c6fbae461fce
srlun <qwe30108@gmail.com>
2026-01-25 18:43:30 +0800

    二次更新

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./React-week2/",
+  //開發中、產品路徑
+  base: process.env.NODE_ENV === "production" ? "/React-week2/" : "/",
   plugins: [react()],
 });
